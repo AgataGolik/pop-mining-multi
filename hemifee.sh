a7c67fe1093e07f69ce4ece833056ad330f65ef8
Agat Golik <105146311+AgataGolik@users.noreply.github.com>
2024-10-24 13:28:42 +0200

    Update hemifee.sh

--- hemifee.sh
+++ hemifee.sh
@@ -16,7 +16,7 @@ restart_service() {
             return 0
         else
             attempts=$((attempts + 1))
-            show \"Failed to restart \$service_name (Attempt \$attempts/\$max_attempts). Retrying in 15 seconds...\"
+            show "Failed to restart $service_name (Attempt $attempts/$max_attempts). Retrying in 15 seconds..."
             sleep 15
         fi
     done
